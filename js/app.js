b9a03d67f2c7c9b3687357ec4ad9d4925ee50b88
critter1996 <109566820+critter1996@users.noreply.github.com>
2023-03-16 21:23:08 -0400

    update js

--- js/app.js
+++ js/app.js
@@ -2,17 +2,61 @@
 document.addEventListener('DOMContentLoaded', () => {
 
 //variables   
-const keys = document.getElementById('qwerty');
+const keys = document.querySelectorAll('qwerty');
 let phrase = document.getElementById('phrase');
-
+const btnReset = document.querySelector('.btn__reset');
 let missed = 0;
 let overlay = document.querySelector('#overlay');
+let phraseList = document.getElementById('#phraseList')
 
+//listen for the start game button to be pressed 
+btnReset.addEventListener('click', () => {
+  overlay.style.display = 'none'; 
+});
 
 //array named phrases
-//const phrases = ['butterfly', 'cat', 'igloo', 'tundra', 'kitchen'];
+const phrases = [
+  'Rain clouds', 
+  'Cats are cool', 
+  'Wild goose chase', 
+  'In a pickle', 
+  'Later gator'
+];
 
 
+//Create a getRandomPhraseAsArray function.
+function getRandomPhraseAsArray(arr){
+    //do stuff to any arr that is passed in 
+    for ( let i = 0; i < phrases.length; i++) {
+      Math.floor(Math.random(phrases[i])* phrases.length)
+      console.log(phrases[i])
+    }
+} 
+
+//call the above function
+getRandomPhraseAsArray(phrases);
+
+
+
+//adds the letters of a string to the display
+function addPhraseToDisplay(arr){
+  // do stuff any arr that is passed in, and add to `#phrase ul`
+  let letters = '';
+  for ( let i = 0; i < arr.length; i++ ) {
+    letters += `<li>${ arr[i] }</li>`;
+
+  }
+  return letters;
+}
+document.querySelector('#phrase').innerHTML = `<ul>${addPhraseToDisplay(phrases)}</ul>`;
+
+
+
+phrases.forEach((item)=>{
+  let li = document.createElement("li");
+  li.innerText = item;
+  list.appendChild(li);
+})
 
 
 //return a random phrase from an array
@@ -20,10 +64,6 @@ let overlay = document.querySelector('#overlay');
 
 // }
 
-//adds the letters of a string to the display
-// const addPhraseToDisplay = arr => {
-
-// }
 
 //check if a letter is in the phrase
 // const checkLetter = button => {
@@ -35,60 +75,13 @@ let overlay = document.querySelector('#overlay');
 
 // }
 
-//listen for the start game button to be pressed 
-const btnReset = document.querySelector('.btn__reset');
-btnReset.addEventListener('click', () => {
-  overlay.style.display = 'none'; 
-});
 
 
-// const search = document.getElementById('search');
-// search.addEventListener('keyup', e => {
-//     let currentValue = e.target.value.toLowerCase();
-//     let info = document.querySelectorAll('[data-caption]');
-
-//     info.forEach(card => {
-//         let text = card.getAttribute('data-caption');
-//         if (text.toLowerCase().includes(currentValue)) {
-//             card.parentNode.style.display = 'block';
-//             console.log(currentValue);
-//         } else {
-//             card.parentNode.style.display = 'none';
-//         }
-//     });
-// });
 
 //listen for the on-screen keyboard to be clicked
-// qwerty.addEventListener('click', e => {
-
+// keys.addEventListener('click', () => {
+//this is a function
 // });
-
-
-
-
-
-
-
-
-
-
-
-console.log(btnReset);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
